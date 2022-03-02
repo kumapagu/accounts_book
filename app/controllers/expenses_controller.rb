55ce71fff3435f76e4cc4575cb0a60a412a6b3fa
kumapagu <kmno_555@yahoo.co.jp>
2022-03-02 13:45:14 +0900

    コントローラーの設定

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,19 @@
 class ExpensesController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
