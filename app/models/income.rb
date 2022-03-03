0ccda44cf1cc2d888ee6ec110e29d330376c2d09
kumapagu <kmno_555@yahoo.co.jp>
2022-03-03 21:52:13 +0900

    収入モデルのバリデーションとアソシエーションの設定

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,2 +1,10 @@
 class Income < ApplicationRecord
+  validates :date, presence: true
+  validates :amount, presence: true
+  validates :income_item_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :income_item
 end
