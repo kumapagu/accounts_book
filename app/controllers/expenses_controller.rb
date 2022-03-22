f8160f2ef69361a6543396f1488722df3a694eb3
mikaymgc <mk25f.y@gmail.com>
2022-03-22 22:57:43 +0900

    Chartkickの導入

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,7 @@
 class ExpensesController < ApplicationController
+  def index
+  end
+  
   def show
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'incomes#index'
-  resources :incomes, :expenses, except: [:index]
+  resources :incomes, :expenses
 
   devise_for :users
 end
