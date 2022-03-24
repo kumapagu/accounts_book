2f90e609b9e232a14ef760ae1bf19421338c3c04
kumapagu <kmno_555@yahoo.co.jp>
2022-03-24 13:28:52 +0900

    cardを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: 'incomes#index'
-  resources :incomes, :expenses, except: [:index]
+  resources :incomes, :expenses, except: [:index] do
+
+    collection do
+      get 'card'
+    end
+  end
 
   devise_for :users
 end
