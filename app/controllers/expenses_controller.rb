2dce6993c244252e7f492ecad63e8e406268fe42
mikaymgc <mk25f.y@gmail.com>
2022-04-05 22:07:38 +0900

    グラフデータ取得

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,12 @@
 class ExpensesController < ApplicationController
+
+  
   def index
+      # @incomes=Income.where("date = ?",)where(['date LIKE ?',  "%#{search}%"])
+    # クエリストリングがあればTimeオブジェクトに変換、ない場合は現在の時刻を取得
+    @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
+    # 取得した時刻が含まれる月の範囲のデータを取得
+    @incomes = Income.where(date: @month.all_month)
   end
   
   def show
@@ -25,4 +32,5 @@ class ExpensesController < ApplicationController
   def expense_params
     params.require(:expense).permit(:date, :amount, :expenditure_item_id, :memo).merge(user_id: current_user.id)
   end
+
 end
